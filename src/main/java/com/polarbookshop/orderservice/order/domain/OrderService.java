f0bb21b337141b7a0dca6f8448c441d5c2147f9c
George Botos <george.botos.no1@gmail.com>
2024-02-13 21:16:19 +0000

    OrderService.java -> added

---
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.orderservice.order.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+	private final OrderRepository orderRepository;
+
+	public Flux<Order> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Mono<Order> submitOrder(String isbn, int quantity) {
+		return Mono.just(buildRejectedOrder(isbn, quantity))
+				       .flatMap(orderRepository::save);
+	}
+
+	public static Order buildRejectedOrder(String isbn, int quantity) {
+		return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
+	}
+}
